ffd2d64b0e4e69fa0bb703914d33bbe4573f5f3c
James Neill <leprechaun1066@gmail.com>
2013-01-12 05:43:38 +0000

    Removed some unnecessary imports.

--- src/igpay/atinlay/IgpayAtinlay.java
+++ src/igpay/atinlay/IgpayAtinlay.java
@@ -1,14 +1,7 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package igpay.atinlay;
 
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.*;
 
 public class IgpayAtinlay extends JFrame{
